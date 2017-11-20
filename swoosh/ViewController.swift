7b9f7cd8820a0a802aadd159c1d67e1f59240176
Andreas Landerer <andreas.landerericloud.com>
2017-11-20 10:21:35 +0100

    Lecture 40: How you could arrange subviews programmatically.

--- swoosh/ViewController.swift
+++ swoosh/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillLayoutSubviews() {
+        let x = (self.view.frame.size.width - self.swooshLogo.frame.size.width) / 2
+        self.swooshLogo.frame = CGRect(x: x,
+                                       y: 50.0,
+                                       width: self.swooshLogo.frame.width,
+                                       height: self.swooshLogo.frame.height)
+        self.backgroundImage.frame = self.view.frame
     }
-
-
 }
 
