fe7f9237d13e05c1c3c7672a6e669f18a836ef33
Andreas Landerer <andreas.landerericloud.com>
2017-11-24 21:01:44 +0100

    Lecture 47: Disable Next Button in LeagueViewController and add an action to the separate league buttons

--- swoosh/Controller/LeagueViewController.swift
+++ swoosh/Controller/LeagueViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    var playerModel: Player!
+    
+    @IBOutlet weak var nextButton: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.playerModel = Player()
     }
-    
     
     @IBAction func onClickNext(_ sender: BorderButton) {
         self.performSegue(withIdentifier: "skillViewController", sender: self)
     }
 
+    @IBAction func onClickLeague(_ sender: BorderButton) {
+        self.playerModel.desiredLeague = sender.titleLabel?.text
+        self.nextButton.isEnabled = true
+    }
 }
---
+++ swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Andreas Landerer on 24.11.17.
+//  Copyright © 2017 Zuehlke. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
